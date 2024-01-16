c38e917b6ff6d0ed429d0fa26a54120b97fc5fc4
mkodagoda223 <155290578+mkodagoda223@users.noreply.github.com>
2024-01-16 13:32:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var typed = new Typed("#multi", {
+  strings: ["Web Developer", "Frontend Developer", "Freelancer"],
+  typeSpeed: 100,
+  loop: true,
+});
+//   const homeBtn = document.getElementById("home");
+
+//   homeBtn.onclick = function() {
+//     window.location.href = "index.html";
+//   };
+
+//   const aboutBtn = document.getElementById("about");
+
+//   homeBtn.onclick = function() {
+//     window.location.href = "about.html";
+//   };
+// document.addEventListener('DOMContentLoaded', function () {
+//   // Get all navigation links
+//   var navLinks = document.querySelectorAll('#navLinks a');
+
+//   // Add click event listener to each navigation link
+//   navLinks.forEach(function (link) {
+//       link.addEventListener('click', function (event) {
+//           // Allow the default behavior (navigation to the specified URL)
+//           // This will automatically take the user to the new page
+//           return true;
+//       });
+//   });
+// });
